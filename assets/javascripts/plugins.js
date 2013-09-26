3e753d896a795033a228a2cc6c0a22eafc0c6794
teppei machida <machidanohimitsu@gmail.com>
2013-09-26 19:10:06 +0900

    build with middleman

---
+++ assets/javascripts/plugins.js
@@ -0,0 +1,1 @@
+window.console&&console.log||function(){for(var o=function(){},e=["assert","clear","count","debug","dir","dirxml","error","exception","group","groupCollapsed","groupEnd","info","log","markTimeline","profile","profileEnd","markTimeline","table","time","timeEnd","timeStamp","trace","warn"],n=e.length,r=window.console={};n--;)r[e[n]]=o}();
